6ea4d1955cf7d8761f9b804cca0cd5fd9101a939
Cmarsman <chris_marsden@yahoo.com>
2024-04-29 21:25:41 -0500

    added circle method

---
+++ Module1Exercise/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Module1Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            //double radius = 5.0;
+            //double area = CircleAreaMethod(radius);
+            //Console.WriteLine("The area of the circle is: " + area);
+
+            Console.WriteLine("Please enter the radius of the circle:");
+            double radius = double.Parse(Console.ReadLine());
+            double area = CircleAreaMethod(radius);
+            Console.WriteLine("The area of the circle is: " + area);
+        }
+        
+        static double CircleAreaMethod(double radius)
+        {
+            return Math.PI * radius * radius;
+        }
+            
+    }
+}
