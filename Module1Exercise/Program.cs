1756706015f28f65c00b5ee10e3d99c2e616c02f
Cmarsman <chris_marsden@yahoo.com>
2024-04-30 21:10:54 -0500

    made changes to exercise

--- Module1Exercise/Program.cs
+++ Module1Exercise/Program.cs
@@ -1,7 +1,11 @@
-﻿namespace Module1Exercise
-{
-    internal class Program
+﻿using System.Security.Principal;
+
+namespace Module1Exercise
+
+    class Program
     {
+        static double baseLength; 
+        
         static void Main(string[] args)
         {
             //Console.WriteLine("Hello, World!");
@@ -22,18 +26,41 @@
             double height = double.Parse(Console.ReadLine());
             double triangleArea = TriangleAreaMethod(baseLength, height);
             Console.WriteLine("The area of the triangle is: " + triangleArea);
+
             //Area of the Rectangle 
+            Console.WriteLine("\nPlease enter the Length of rectangle.");
+            double Length = double.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the width of rectangle.");
+            double width = double.Parse(Console.ReadLine());
+            double rectangleArea = RectangleAreaMethod(Length, width);
+            Console.WriteLine("The area of the rectangle is: " + rectangleArea);
+
             //Area of the Square
+            Console.WriteLine("\nPlease enter a side of the square.");
+            double side = double.Parse(Console.ReadLine());
+            double squareArea = SquareAreaMethod(side * side);
+            Console.WriteLine("The area of the square is: " + squareArea);
         }
         
         static double CircleAreaMethod(double radius)
         {
             return Math.PI * radius * radius;
         }
-            
+
         static double TriangleAreaMethod(double baseLength, double height)
         {
             return (baseLength * height) / 2;
         }
+
+        static double RectangleAreaMethod(double length, double width)
+        {
+            return (baseLength * width) / 2;
+        }
+
+        static double SquareAreaMethod(double side)
+            {
+            return (side * side);
+            }
+        }
     }
-}
+
